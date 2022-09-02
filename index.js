f0ecd2268d6bd401466f59554b3f486de139c94d
Teana Zhong <Teanawk@gmail.com>
2022-09-01 22:21:46 -0400

    commit tree c36b363fe0b0d7a57eff15d1ad652ea0f8c9a527

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers){
+    let firstTwoDrivers = drivers.slice(0,2);
+
+    return firstTwoDrivers; }
+
+const returnLastTwoDrivers = function (drivers) {
+    let lastTwoDrivers = drivers.slice(2,4);
+    return lastTwoDrivers;
+}
+
+let selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (value){
+    return function(fare) {
+        return fare * value;
+    }
+}
+
+const fareDoubler= createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, a) {
+    return a(drivers)
+    ;
+}
+
+
+
+
+// const ret = selectDifferentDrivers(drivers, firstTwoDrivers)
